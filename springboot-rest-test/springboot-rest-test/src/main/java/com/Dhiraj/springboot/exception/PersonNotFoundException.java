9124673b751eedc264464c871d95b06eabc14e66
Dhiraj093328 <162727053+Dhiraj093328@users.noreply.github.com>
2026-03-18 13:39:39 +0530

    Exception

---
+++ springboot-rest-test/springboot-rest-test/src/main/java/com/Dhiraj/springboot/exception/PersonNotFoundException.java
@@ -0,0 +1,9 @@
+package com.Dhiraj.springboot.exception;
+
+public class PersonNotFoundException extends RuntimeException {
+	public PersonNotFoundException (String message)
+	{
+		super(message);
+	}
+
+}
--- springboot-rest-test/springboot-rest-test/src/main/java/com/Dhiraj/springboot/service/PersonService.java
+++ springboot-rest-test/springboot-rest-test/src/main/java/com/Dhiraj/springboot/service/PersonService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Dhiraj.springboot.exception.PersonNotFoundException;
 import com.Dhiraj.springboot.model.Person;
 import com.Dhiraj.springboot.repository.PersonRepository;
 
@@ -13,41 +14,36 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class PersonService {
-	
-	@Autowired
-	private PersonRepository personRepository;
-	
-	public Person createPerson(Person person)
-	{
-		return personRepository.save(person);
-	}
-	
-	public Person getPersonById(Long id)
-	{
-		return personRepository.findById(id).orElse(null);
-	}
-	public List<Person> getAllPerson()
-	{
-		return personRepository.findAll();
-	}
-	
-	public String deletePerson(Long id)
-	{
-		personRepository.deleteById(id);
-		return "Person deleted successfully";
-	}
-	public Person updatePerson(Long id,Person person)
-	{
-		Person existingPerson=personRepository.findById(id).orElse(null);
-		
-		if(existingPerson!=null)
-		{
-			existingPerson.setName(person.getName());
-			existingPerson.setEmail(person.getEmail());
-			return personRepository.save(existingPerson);
-		}
-		return null;
-		
-	}
-	
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person createPerson(Person person) {
+        return personRepository.save(person);
+    }
+
+    public Person getPersonById(Long id) {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException("Person not found with id: " + id));
+    }
+
+    public List<Person> getAllPerson() {
+        return personRepository.findAll();
+    }
+
+    public String deletePerson(Long id) {
+        personRepository.deleteById(id);
+        return "Person deleted successfully";
+    }
+
+    public Person updatePerson(Long id, Person person) {
+        Person existingPerson = personRepository.findById(id).orElse(null);
+
+        if (existingPerson != null) {
+            existingPerson.setName(person.getName());
+            existingPerson.setEmail(person.getEmail());
+            return personRepository.save(existingPerson);
+        }
+        return null;
+    }
 }
